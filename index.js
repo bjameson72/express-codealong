aaeb4a7311d592b443dd19949affbd947d65850f
Brock Jameson <bjameson@talentpath.com>
2019-03-04 11:37:14 -0800

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const commentsRouter = require("./routes/comments");
 
 const app = express();
@@ -17,6 +18,9 @@ app.use(
     extended: false,
   })
 );
+
+// cors middleware
+app.use(cors());
 
 // logger middleware
 // app.use(logger);
